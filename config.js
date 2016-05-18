fb43fd8e228c180dbffcbbf5b1a98b769b7e3ac9
Rowan Manning <rowan.manning@ft.com>
2016-05-18 10:59:51 +0100

    Abstract the logging into config

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
 	environment: process.env.NODE_ENV || 'development',
+	log: console,
 	logLevel: process.env.LOG_LEVEL || 'info',
 	port: process.env.PORT || 8080
 };
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const config = require('./config');
 const imageService = require('./lib/image-service');
 
 imageService(config).catch(error => {
-	console.error(error.stack); // TODO use proper logging
+	config.log.error(error.stack);
 	process.exit(1);
 });
