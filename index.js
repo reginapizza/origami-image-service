400c424d9e29ba7dcc4f513c6ce293fb52745a3c
Jake Champion <me@jakechampion.name>
2016-10-27 12:08:48 +0100

    Enable application to use multiple processes (#112)
    * Add throng to handle running multiple processes
    
    * Fix bug: Log to stderr if service rejects

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ require('dotenv').load({
 	silent: false
 });
 
-const config = require('./config');
-const imageService = require('./lib/image-service');
+const throng = require('throng');
 
-imageService(config).catch(error => {
-	config.log.error(error.stack);
-	process.exit(1);
+throng({
+  workers: process.env.WEB_CONCURRENCY || 1,
+  start: (id) => {
+		console.log(`Started worker ${ id }`);
+
+		const config = require('./config');
+		const imageService = require('./lib/image-service');
+
+		imageService(config).catch(error => {
+			config.error(error.stack);
+			process.exit(1);
+		});
+	}
 });
