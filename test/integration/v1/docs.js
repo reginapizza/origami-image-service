4e3710fb83fee934404ba46b7df760cd1b4e9ec4
Rowan Manning <accounts@rowanmanning.co.uk>
2016-10-31 10:24:14 +0000

    Redirect to v1 instead of showing an error (#123)
    I think this is nicer behaviour - it allows users to more easily switch
    between versions of the service while they're testing it. It can change
    to a 410 Gone status once v1 is decommissioned.

--- test/integration/v1/docs.js
+++ test/integration/v1/docs.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const itRespondsWithContentType = require('../helpers/it-responds-with-content-type');
+const itRespondsWithHeader = require('../helpers/it-responds-with-header');
 const itRespondsWithStatus = require('../helpers/it-responds-with-status');
 const setupRequest = require('../helpers/setup-request');
 
 describe('GET /v1/', function() {
 	setupRequest('GET', '/v1/');
-	itRespondsWithStatus(501);
-	itRespondsWithContentType('text/html');
+	itRespondsWithStatus(301);
+	itRespondsWithHeader('Location', 'https://image.webservices.ft.com/v1/');
+});
+
+describe('GET /v1/images/raw/fticon:cross?source=test', function() {
+	setupRequest('GET', '/v1/images/raw/fticon:cross?source=test');
+	itRespondsWithStatus(301);
+	itRespondsWithHeader('Location', 'https://image.webservices.ft.com/v1/images/raw/fticon:cross?source=test');
 });
