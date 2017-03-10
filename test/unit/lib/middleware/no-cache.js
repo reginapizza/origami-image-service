9d6e9cb11709bbfbc76fed45148fbc165f75eaf0
Jake Champion <me@jakechampion.name>
2017-03-10 10:51:32 +0000

    Do not cache the purge endpoints (#236)

---
+++ test/unit/lib/middleware/no-cache.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const assert = require('proclaim');
+require('sinon-as-promised');
+
+describe('lib/middleware/no-cache', () => {
+	let origamiService;
+	let noCache;
+
+	beforeEach(() => {
+		origamiService = require('../../mock/origami-service.mock');
+
+		noCache = require('../../../../lib/middleware/no-cache');
+	});
+
+	it('exports a function', () => {
+		assert.isFunction(noCache);
+	});
+
+	describe('noCache()', () => {
+		let middleware;
+
+		beforeEach(() => {
+			middleware = noCache();
+		});
+
+		it('returns a middleware function', () => {
+			assert.isFunction(middleware);
+		});
+
+		describe('middleware(request, response, next)', () => {
+
+			let middleware;
+
+			beforeEach(() => {
+				middleware = noCache();
+			});
+
+			it('set Cache-Control header to not cache', () => {
+				middleware(origamiService.mockRequest, origamiService.mockResponse, origamiService.mockNext);
+				assert.calledWithExactly(origamiService.mockResponse.set, 'Cache-Control', 'private, max-age=0, no-cache');
+			});
+
+			it('set Surrogate-Control to not cache', () => {
+				middleware(origamiService.mockRequest, origamiService.mockResponse, origamiService.mockNext);
+				assert.calledWithExactly(origamiService.mockResponse.set, 'Surrogate-Control', 'private, max-age=0, no-cache');
+			});
+
+			it('calls `next`', () => {
+				middleware(origamiService.mockRequest, origamiService.mockResponse, origamiService.mockNext);
+				assert.called(origamiService.mockNext);
+			});
+		});
+	});
+});
